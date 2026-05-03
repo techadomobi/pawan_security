d41d10bda9e9799dbda8e450d7e4adb35c854818
Yashika soni <soniyashika164@gmail.com>
2026-05-03 06:46:25 +0000

    logo

--- artifacts/pawan-security/src/components/layout/Navbar.tsx
+++ artifacts/pawan-security/src/components/layout/Navbar.tsx
@@ -38,7 +38,7 @@ export function Navbar() {
           <img
             src={isScrolled ? "/logo_white_sm.png" : "/logo.png"}
             alt="Smart CCTV India"
-            className="h-14 w-auto transition-transform duration-300 group-hover:scale-105 sm:h-16"
+            className="h-16 w-auto transition-transform duration-300 group-hover:scale-105 sm:h-20"
           />
         </Link>
 
